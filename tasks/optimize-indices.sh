7a950574c872d813c7b94df1e0b1b74378e1f8ba
Michael Gelfand <michael@senexx.com>
2015-12-27 14:25:12 +0200

    allow passsing multiple indeces prefixes

--- tasks/optimize-indices.sh
+++ tasks/optimize-indices.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curator --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT optimize --max_num_segments 1 indices --prefix $INDICES_PREFIX --older-than 1 --time-unit days --timestring '%Y.%m.%d'
+for INDICES_PREFIX in $INDICES_PREFIXES; do
+  curator --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT optimize --max_num_segments 1 indices --prefix $INDICES_PREFIX --older-than 1 --time-unit days --timestring '%Y.%m.%d'
+done
--- tasks/purge-old-indices.sh
+++ tasks/purge-old-indices.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ -n "$MAX_INDEX_AGE" ]; then
-  /usr/bin/curator --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT delete indices --older-than $MAX_INDEX_AGE --time-unit days --prefix $INDICES_PREFIX  --timestring '%Y.%m.%d'
+  for INDICES_PREFIX in $INDICES_PREFIXES; do
+    /usr/bin/curator --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT delete indices --older-than $MAX_INDEX_AGE --time-unit days --prefix $INDICES_PREFIX  --timestring '%Y.%m.%d'
+  done
 else
   echo "Skip purging old indices. MAX_INDEX_AGE is not set."
 fi
