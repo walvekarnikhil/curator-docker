f020463da873d75e75fd308b9a29bdcb8338f65f
Nikhil Walvekar <nikhil.walvekar@synerzip.com>
2017-10-18 14:54:14 -0700

    Added support for SSL Upgraded curator version.

--- tasks/purge-old-indices.sh
+++ tasks/purge-old-indices.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -n "$MAX_INDEX_AGE" ]; then
-  for INDICES_PREFIX in $INDICES_PREFIXES; do
-    /usr/bin/curator --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT delete indices --older-than $MAX_INDEX_AGE --time-unit days --prefix $INDICES_PREFIX  --timestring '%Y.%m.%d'
-  done
+  #for INDICES_PREFIX in $INDICES_PREFIXES; do
+    /usr/bin/curator --config /etc/periodic/config/config.yml /etc/periodic/config/actions.yml
+  #done
 else
   echo "Skip purging old indices. MAX_INDEX_AGE is not set."
 fi
